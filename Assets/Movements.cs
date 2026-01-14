bd8f881cefe56118cf58461eb94ab6ca5b75a1e5
CoolElias73 <Elias.o.a.mattsson@gmail.com>
2026-01-14 11:28:39 +0100

    Movements and camera follow
    made the camera follow the goat vertically and move side to side and jump

--- Assets/Movements.cs
+++ Assets/Movements.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Movements : MonoBehaviour
+public class SimpleMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    public float groundDistance = 0.6f;
+
+    private Rigidbody2D rb;
+    private float horizontal;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current != null)
+        {
+            horizontal =
+                (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed ? -1 : 0) +
+                (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed ? 1 : 0);
+
+            if (Keyboard.current.spaceKey.wasPressedThisFrame && IsGrounded())
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, groundDistance);
     }
 }
