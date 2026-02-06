4e9380b61bb9a3a3a58d44d167f73bbe954ca74c
sejnse <emiliano.svard@elev.ga.lbs.se>
2026-02-06 09:22:20 +0100

    Power texture

--- Assets/PoweruponTouch.cs
+++ Assets/PoweruponTouch.cs
@@ -5,6 +5,7 @@ public class Powerup : MonoBehaviour
     public float speedIncrease = 1f;
     public float jumpIncrease = 2f;
 
+    public AudioClip pickupSound;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -13,6 +14,11 @@ public class Powerup : MonoBehaviour
             if (player != null)
             {
                 player.IncreaseStats(speedIncrease, jumpIncrease);
+            }
+
+            if (pickupSound != null)
+            {
+                AudioSource.PlayClipAtPoint(pickupSound, transform.position, 3f);
             }
 
             Destroy(gameObject);
